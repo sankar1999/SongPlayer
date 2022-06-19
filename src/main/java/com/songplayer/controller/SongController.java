140436763b5db6487d77fa1b2ac25d312aa8c4c0
sankar1999 <sankarsathya1973@gmail.com>
2022-06-20 00:59:02 +0530

    First Commit

---
+++ src/main/java/com/songplayer/controller/SongController.java
@@ -0,0 +1,44 @@
+package com.songplayer.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+@RestController
+public class SongController {
+
+    @GetMapping("play")
+    public String playSong() throws LineUnavailableException, UnsupportedAudioFileException, IOException {
+
+        Scanner scanner = new Scanner(System.in);
+
+        File file = new File("Songs/Sorry_Test.wav");
+        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file);
+        Clip clip = AudioSystem.getClip();
+        clip.open(audioInputStream);
+
+        clip.start();
+
+        String response = scanner.next();
+
+        return "Song Played Successfully...";
+
+    }
+
+//    public SongController() throws LineUnavailableException, UnsupportedAudioFileException, IOException {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        File file = new File("Sorry_Test.wav");
+//        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file);
+//        Clip clip = AudioSystem.getClip();
+//        clip.open(audioInputStream);
+//
+//        clip.start();
+//
+//        String response = scanner.next();
+//    }
+}
